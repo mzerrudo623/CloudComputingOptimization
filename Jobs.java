5c4ad65a9b0c76bb9246a7ddd9bedbdabf3dc3ce
mzerrudo623 <mzerrudo2@toromail.csudh.edu>
2016-08-04 13:27:15 -0700

    Revert "Add files via upload"
    This reverts commit 824ebef2dfc3e933a8fb2144dcb0722fcea12051.

--- Jobs.java
+++ Jobs.java
@@ -3,24 +3,17 @@
  * Research Project
  * Dr. Chatterjee
  */
-public class Jobs implements Comparable<Jobs>{
-	private int jobNum;
+public class Jobs{
 	private int jobTime;
-	private int reqMem;		//required memory to do job
-	private int reqProc;	//required processor speed to do job
-	public static boolean batch;
-	public static boolean service;
+	private int jobNum;
 	
 	public Jobs(){
 		jobTime = 5000;
 	}
 	
-	//Jobs(job number, job delay, required memory, required processor speed)
-	public Jobs(int num1, int num2, int num3, int num4){
-		jobNum = num1;
-		jobTime = num2;
-		reqMem = num3;
-		reqProc = num4;
+	public Jobs(int num1, int num2){
+		jobTime = num1;
+		jobNum = num2;
 	}
 	
 	public int getJobTime(){
@@ -29,32 +22,6 @@ public class Jobs implements Comparable<Jobs>{
 	
 	public int getJobNum(){
 		return jobNum;
-	}
-	
-	public int getReqMemory(){
-		return reqMem;
-	}
-	
-	public int getReqProcessor(){
-		return reqProc;
-	}
-	
-	//returns 0 if batch, 1 if service
-	public int checkJobType(){
-		if (batch == true)
-			return 0;
-		else
-			return 1;
-	}
-	
-	//compares based on job number
-	public int compareTo(Jobs j){
-		if (jobNum < j.getJobNum())
-			return -1;
-		else if (jobNum > j.getJobNum())
-			return 1;
-		else
-			return 0;
 	}
 	
 	/*public void startJob(String name) throws InterruptedException{
